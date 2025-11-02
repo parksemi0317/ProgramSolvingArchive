25df245d7447fc6dee52a4946373b9e1594ba9ec
parksemi0317 <108038296+parksemi0317@users.noreply.github.com>
2025-11-03 07:18:28 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 364 ms, Memory: 120100 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -8,37 +8,32 @@ print = sys.stdout.write
 
 N, K = map(int, input().rstrip().split())
 
-q = [] # 시간, 현재 위치
+q = [] # (시간, 현재 위치) 꼴의 우선순위 큐
 is_visited = [0 for _ in range(MAX+1)]
 
-
+# 시작 지점 우선 순위 큐에 삽입
 heapq.heappush(q, (0,N))
 
 while 1:
     t, cur = heapq.heappop(q)
 
+    # 목적지에 도달한 경우
     if cur == K:
         print(f"{t}")
         break
-        
-    if  K != 0 and cur % K == 0:
-        temp1 = cur // K
-        while temp1  & 1:
-            temp1 = temp1 >> 1
-        if temp1 == 1:
-            print(f"{t}")
-            break
 
+    # 목적지에 순간 이동으로 접근 가능한 경우 처리
     if cur != 0 and K % cur == 0:
-        temp2 = K // cur
-        while temp2  & 1:
-            temp2 = temp2 >> 1
-        if temp2 == 1:
+        temp = K // cur
+        while temp  & 1:
+            temp = temp >> 1
+        if temp == 1:
             print(f"{t}")
             break
             
     is_visited[cur] = 1
     
+    # 이동 가능 지점 우선 순위 큐에 삽입
     if 2*cur <= MAX and is_visited[2*cur] == 0 :
         heapq.heappush(q, (t, 2*cur))
     if cur + 1 <= MAX and is_visited[cur + 1] == 0:
