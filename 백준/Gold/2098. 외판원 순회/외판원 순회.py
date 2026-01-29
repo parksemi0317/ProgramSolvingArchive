dd796698850522ceb7ee27ae6e47c5bdb9323a29
parksemi0317 <108038296+parksemi0317@users.noreply.github.com>
2026-01-29 15:29:35 +0900

    [Gold I] Title: 외판원 순회, Time: 228 ms, Memory: 122456 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -20,36 +20,47 @@ INF = 1000000 * 20
 
 # [N][ALL_VISITED + 1]
 # 마지막 위치, 전체 방문 여부
+# INF : 미확인, -1 : 도달 불가
 dp = [[INF for _ in range(ALL_VISITED+1)] for _ in range(N)]
 
-# ==================== 탐색 함수 ====================
+# ===================== BFS ====================
 
 q = deque([])
 
-# 1에서 이동 가능한 지점들 표시
-for idx, dist in enumerate(bridges[0]):
-    if dist != 0:
-        bitMask = 0b1 << idx
-        dp[idx][bitMask] = dist
-        q.append((idx, bitMask))
+# 현재 위치에서 다시 원점으로 돌아가는데까지 남은 거리 반환 함수
+# 돌아갈 수 없는 경우 -1 반환
+def dfs(currentIdx, bitMask):
+    global ALL_VISITED,INF, dp, bridges
 
-
-while q:
-    currentIdx , bitMask = q.popleft()
-    curDistance = dp[currentIdx][bitMask]
+    # 이미 값을 구한 경우 (Dp)
+    if dp[currentIdx][bitMask] != INF:
+        return dp[currentIdx][bitMask]
     
+    if bitMask == ALL_VISITED:
+        # 모든 경로를 전부 방문한 경우
+        result = bridges[currentIdx][0] if bridges[currentIdx][0] else -1
+        dp[currentIdx][bitMask] = result
+        return result
+
+    # 다음 경로로 이동하기
+    result = INF
     for distIdx, dist in enumerate(bridges[currentIdx]):
         distBitMask = 0b1 << distIdx
 
         # 연결되어 있으며, 아직 방문하지 않은 경우
         if dist != 0 and bitMask & distBitMask == 0:
             newBitMask = bitMask | distBitMask
-            
-            if curDistance + dist < dp[distIdx][newBitMask]:
-                dp[distIdx][bitMask | distBitMask] = curDistance + dist
-                q.append((distIdx, bitMask | distBitMask))
 
-print(f"{dp[0][ALL_VISITED]}")
+            nextDfs = dfs(distIdx, newBitMask)
+            if nextDfs != -1:
+                result = min(result, nextDfs + dist)
+
+    if result == INF:
+        result  = -1
+    dp[currentIdx][bitMask] = result
+    return result
+
+print(f"{dfs(0, 0b1)}")
                 
 
 
