6c9a85213a81cf192ab78695cd2d603016a7867e
parksemi0317 <108038296+parksemi0317@users.noreply.github.com>
2026-05-08 01:23:08 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 9.11 MB, 시간: 0.02 ms
+메모리: 9.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 08일 01:20:45
+2026년 05월 08일 01:23:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,15 +1,16 @@
 def solution(name):
     N = len(name)
     print("N :", N)
-    
+    # ========== 알파벳 바꾸는 횟수 구하기
     up_down = 0
     for i, c in enumerate(name):
         if c != "A":
             up_down += min(ord(c)-65, 90-ord(c)+1)
-    print("up_down : ", up_down)
-    if up_down == 0:
+            
+    if up_down == 0: # 바꿀 알파벳이 없는 경우
         return 0
-        
+    
+    # ========== 연속된 A 조합 구하기
     a_sets = []
     start_i = -1
     for i in range(N):
@@ -25,10 +26,14 @@ def solution(name):
         a_sets.append((start_i, N-1))
     
     print("a_sets :", a_sets)
-    left_right = N -1
+    
+    # ========== 연속된 A에 대하여 해당 구역 안지나가는 경우 고려하기
+    
+    left_right = N -1 # 오른쪽으로 쭉 이동하는 경우
+    
     for s,e in a_sets:
-        r_l = (s-1 if s > 0 else 0) * 2 + ((N-1) - e)
-        l_r = ((N-1) - e) * 2 + (s-1 if s > 0 else 0) 
+        r_l = (s-1 if s > 0 else 0) * 2 + ((N-1) - e) # 오른쪽으로 갔다가 왼쪽으로 돌아가는 경우
+        l_r = ((N-1) - e) * 2 + (s-1 if s > 0 else 0) # 왼쪽으로 갔다가 오른쪽으로 돌아오는 경우
         left_right = min(left_right, l_r, r_l)
         print(f"s:{s}, e:{e} => r_l:{r_l}, l_r:{l_r}")
     print("left_right :",left_right)                  
